0f59a5f2130ff65178668fd84aed4405f278a161
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-15 14:14:43 +0300

    Puzzle solved

--- min_max_search/Program.cs
+++ min_max_search/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Начало программы");
+
+int[] list = {5, 1, 3, 4, 2};
+
+int i = 0;
+int maxValueIndex = 0;
+int minValueIndex = 0;
+int size = list.Length;
+
+Console.Write("Размер массива: ");
+Console.WriteLine(size);
+
+while (i<size) {
+    if (list[i] > list[maxValueIndex]) {
+        maxValueIndex = i;
+    }
+    else if (list[i] < list[minValueIndex])  {
+        minValueIndex = i;
+    }
+    i++;
+}
+
+Console.Write("Индекс максимального элемента в массиве: ");
+Console.WriteLine(maxValueIndex);
+Console.Write("Индекс минимального элемента в массиве: ");
+Console.WriteLine(minValueIndex);
